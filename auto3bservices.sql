477fd0635abc05d10e9f2c76987344b0d320d972
ridaovic <mlle.haji@gmail.com>
2015-12-21 09:25:53 +0000

    ajout de la table factures

--- auto3bservices.sql
+++ auto3bservices.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 05 Décembre 2015 à 21:53
--- Version du serveur :  5.6.26
--- Version de PHP :  5.6.12
+-- Généré le :  Ven 18 Décembre 2015 à 09:44
+-- Version du serveur :  10.0.17-MariaDB
+-- Version de PHP :  5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,10 +23,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `factures`
+--
+
+CREATE TABLE `factures` (
+  `num_fact` varchar(25) NOT NULL,
+  `date_fact` date NOT NULL,
+  `nom` varchar(25) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `immatriculation` varchar(25) NOT NULL,
+  `marque_voiture` varchar(25) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `qte` int(10) NOT NULL,
+  `etat_facture` varchar(10) NOT NULL,
+  `prix` float NOT NULL,
+  `montant` float NOT NULL,
+  `total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `factures`
+--
+
+INSERT INTO `factures` (`num_fact`, `date_fact`, `nom`, `prenom`, `immatriculation`, `marque_voiture`, `designation`, `qte`, `etat_facture`, `prix`, `montant`, `total`) VALUES
+('00001', '2015-12-03', 'Client_nom', 'Client_prenom', 'immatriculation_1', 'volvo', 'designation_1', 1, '', 1500, 1500, 1500);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `produits`
 --
 
-CREATE TABLE IF NOT EXISTS `produits` (
+CREATE TABLE `produits` (
   `ref` varchar(250) NOT NULL DEFAULT '',
   `nom` varchar(250) NOT NULL,
   `prix` float NOT NULL,
@@ -39,8 +67,11 @@ CREATE TABLE IF NOT EXISTS `produits` (
 --
 
 INSERT INTO `produits` (`ref`, `nom`, `prix`, `qte`, `qte_min`) VALUES
-('zzzz', 'zzzzzz', 120, 5, 6),
-('dds', 'sssssssshh', 120, 6, 6);
+('zzzz', 'zzzzzz', 120, 1, 6),
+('dds', 'sssssssshh', 123, 3, 15),
+('f', 'd', 46, 14, 12),
+('mmmmmmmm', 'mmmmmmmmmm', 20, 15, 5),
+('okok', 'okok', 222, 12, 2);
 
 -- --------------------------------------------------------
 
@@ -48,14 +79,14 @@ INSERT INTO `produits` (`ref`, `nom`, `prix`, `qte`, `qte_min`) VALUES
 -- Structure de la table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `users`
@@ -67,6 +98,12 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `nom`, `prenom`) VAL
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `factures`
+--
+ALTER TABLE `factures`
+  ADD PRIMARY KEY (`num_fact`);
 
 --
 -- Index pour la table `produits`
@@ -88,7 +125,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
