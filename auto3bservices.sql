680bd6d8b604a14714db4adfe8f13e22281679bf
ridaovic <mlle.haji@gmail.com>
2015-12-23 12:46:16 +0000

    modification des id ==> ajout des autoincrement

--- auto3bservices.sql
+++ auto3bservices.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 22 Décembre 2015 à 23:42
--- Version du serveur :  5.6.26
--- Version de PHP :  5.6.12
+-- Généré le :  Mer 23 Décembre 2015 à 13:45
+-- Version du serveur :  10.0.17-MariaDB
+-- Version de PHP :  5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,21 +26,21 @@ SET time_zone = "+00:00";
 -- Structure de la table `devis`
 --
 
-CREATE TABLE IF NOT EXISTS `devis` (
+CREATE TABLE `devis` (
   `id` int(11) NOT NULL,
   `date_devis` date DEFAULT NULL,
   `date_accord` date DEFAULT NULL,
   `ht` double NOT NULL,
   `hta` double NOT NULL,
   `id_vehicule` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `devis`
 --
 
 INSERT INTO `devis` (`id`, `date_devis`, `date_accord`, `ht`, `hta`, `id_vehicule`) VALUES
-(8, '2015-12-23', '2015-12-31', 20, 30, 7);
+(9, '2015-12-23', '2015-12-31', 20, 30, 7);
 
 -- --------------------------------------------------------
 
@@ -48,8 +48,8 @@ INSERT INTO `devis` (`id`, `date_devis`, `date_accord`, `ht`, `hta`, `id_vehicul
 -- Structure de la table `factures`
 --
 
-CREATE TABLE IF NOT EXISTS `factures` (
-  `num_fact` varchar(25) NOT NULL,
+CREATE TABLE `factures` (
+  `num_fact` int(25) NOT NULL,
   `date_fact` date NOT NULL,
   `designation` varchar(50) NOT NULL,
   `qte` int(10) NOT NULL,
@@ -65,10 +65,10 @@ CREATE TABLE IF NOT EXISTS `factures` (
 --
 
 INSERT INTO `factures` (`num_fact`, `date_fact`, `designation`, `qte`, `etat_facture`, `prix`, `montant`, `total`, `id_vehicule`) VALUES
-('00001', '2015-12-25', '001', 2, 'actives', 1000, 2000, 1000, 7),
-('i123', '2015-12-24', '001', 2, 'active', 1500, 1500, 1000, 1),
-('n123', '2015-12-26', 'test', 4, 'Non payÃ©', 4, 4, 4, 5),
-('n123321', '2015-12-26', 'test', 3, 'PayÃ©', 3, 4, 3, 6);
+(1, '2015-12-25', '001', 2, 'actives', 1000, 2000, 1000, 7),
+(2, '2015-12-24', '001', 2, 'active', 1500, 1500, 1000, 1),
+(3, '2015-12-26', 'test', 4, 'Non payÃ©', 4, 4, 4, 5),
+(4, '2015-12-26', 'test', 3, 'PayÃ©', 3, 4, 3, 6);
 
 -- --------------------------------------------------------
 
@@ -76,7 +76,7 @@ INSERT INTO `factures` (`num_fact`, `date_fact`, `designation`, `qte`, `etat_fac
 -- Structure de la table `produits`
 --
 
-CREATE TABLE IF NOT EXISTS `produits` (
+CREATE TABLE `produits` (
   `ref` varchar(250) NOT NULL DEFAULT '',
   `nom` varchar(250) NOT NULL,
   `prix` float NOT NULL,
@@ -89,6 +89,11 @@ CREATE TABLE IF NOT EXISTS `produits` (
 --
 
 INSERT INTO `produits` (`ref`, `nom`, `prix`, `qte`, `qte_min`) VALUES
+('zzzz', 'zzzzzz', 120, 1, 6),
+('dds', 'sssssssshh', 123, 3, 15),
+('f', 'd', 46, 14, 12),
+('mmmmmmmm', 'mmmmmmmmmm', 20, 15, 5),
+('okok', 'okok', 222, 12, 2),
 ('ref1', 'prod1', 8, 17, 14),
 ('ref2', 'prod2', 6, 9, 3);
 
@@ -98,14 +103,14 @@ INSERT INTO `produits` (`ref`, `nom`, `prix`, `qte`, `qte_min`) VALUES
 -- Structure de la table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `users`
@@ -120,7 +125,7 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `nom`, `prenom`) VAL
 -- Structure de la table `vehicules`
 --
 
-CREATE TABLE IF NOT EXISTS `vehicules` (
+CREATE TABLE `vehicules` (
   `id` int(11) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
@@ -130,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `vehicules` (
   `assurance` varchar(100) NOT NULL,
   `date_entree` date DEFAULT NULL,
   `date_sortie` date DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `vehicules`
@@ -187,17 +192,22 @@ ALTER TABLE `vehicules`
 -- AUTO_INCREMENT pour la table `devis`
 --
 ALTER TABLE `devis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pour la table `factures`
+--
+ALTER TABLE `factures`
+  MODIFY `num_fact` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
